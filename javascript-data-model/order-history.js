cd3d800f00a51d55bacb5fc8f743a7329eec13b3
Steve Portillo <steve.prtll10@gmail.com>
2021-10-27 20:48:13 -0700

    Completed JavaScript Data Model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,74 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    placedOn: '08/04/2020',
+    total: 34,
+    recipient: 'JS Masher',
+    deliveredOn: '08/09/2020',
+    items: [
+      {
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        canReturnUntil: '09/07/2020',
+        price: 31.55
+
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-128057',
+    placedOn: '07/19/2020',
+    total: 44.53,
+    recipient: 'JS Masher',
+    deliveredOn: '07/20/2020',
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        canReturnUntil: '08/19/2020',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    placedOn: '07/04/2020',
+    total: 17.22,
+    recipient: 'JS Masher',
+    deliveredOn: '07/07/2020',
+    items: [
+      {
+        type: 'hardware',
+        title: 'Gamecube Controller Adapter.',
+        manufacturer: 'CLOUDREAM',
+        canReturnUntil: '08/05/2020',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    placedOn: '06/03/2020',
+    total: 138.93,
+    recipient: 'JS Masher',
+    deliveredOn: '07/05/2020',
+    items: [
+      {
+        type: 'hardware',
+        title: 'Gamecube Controller.',
+        manufacturer: 'Nintendo',
+        canReturnUntil: '08/04/2020',
+        price: 94.95
+      },
+      {
+        type: 'book',
+        title: 'The Art of SQL',
+        author: 'Faroult, Stephane',
+        canReturnUntil: '08/04/2020',
+        price: 33.99
+      }
+    ]
+  }
+];
