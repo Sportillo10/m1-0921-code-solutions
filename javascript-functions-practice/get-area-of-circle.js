9b20761e4b41d4ad48ecadf6390f7e2324b6b069
Steve Portillo <steve.prtll10@gmail.com>
2021-10-27 20:41:07 -0700

    Completed JavaScript Functions Practice

---
+++ javascript-functions-practice/get-area-of-circle.js
@@ -0,0 +1,5 @@
+/* exported getAreaOfCircle */
+function getAreaOfCircle(radius) {
+  var circle = Math.PI * radius ** 2;
+  return circle;
+}
---
+++ javascript-functions-practice/get-last-element.js
@@ -0,0 +1,5 @@
+/* exported getLastElement */
+function getLastElement(array) {
+  var element = array.slice(-1)[0];
+  return element;
+}
